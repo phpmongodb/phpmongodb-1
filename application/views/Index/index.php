47a21fd725930095a78e3b6b89d05abb043d4be5
Nanhe <nanhe4it@gmail.com>
2014-04-16 18:54:58 +0530

    Build information

--- application/views/Index/index.php
+++ application/views/Index/index.php
@@ -25,46 +25,12 @@
         <div id="widget1container" class="block-body collapse in">
             <table class="table">
                 <tbody>
+                    <?php foreach($this->data['mongoinfo'] as $k=>$v){ ?>
                     <tr>
-                        <td>Version</td>
-                        <td><?php echo $this->data['mongoinfo']['version']; ?></td>
+                        <td><?php echo $k;?></td>
+                        <td><?php print_r($v);?></td>
                     </tr>
-                    <tr>
-                        <td>gitVersion</td>
-                        <td><?php echo $this->data['mongoinfo']['gitVersion']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>sysInfo</td>
-                        <td><?php echo $this->data['mongoinfo']['sysInfo']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>loaderFlags</td>
-                        <td><?php echo $this->data['mongoinfo']['loaderFlags']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>compilerFlags</td>
-                        <td><?php echo $this->data['mongoinfo']['compilerFlags']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>allocator</td>
-                        <td><?php echo $this->data['mongoinfo']['allocator']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>javascriptEngine</td>
-                        <td><?php echo $this->data['mongoinfo']['javascriptEngine']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>bits</td>
-                        <td><?php echo $this->data['mongoinfo']['bits']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>debug</td>
-                        <td><?php echo $this->data['mongoinfo']['debug']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>maxBsonObjectSize</td>
-                        <td><?php echo $this->data['mongoinfo']['maxBsonObjectSize']; ?></td>
-                    </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
