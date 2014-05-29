f9837b2d1d5d8dc34aa174537a8e99a29d513ef4
Nanhe Kumar <nanhe.kumar@gmail.com>
2014-05-29 18:16:51 +0530

    autocomplete added for db

--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -1,17 +1,18 @@
+<?php $autocomplete= isset(Config::$autocomplete) && Config::$autocomplete==TRUE ?'on':'off';?>
 <div class="dialog">
     <div class="block">
         <p class="block-heading">Sign In</p>
         <div class="block-body">
             <form id="tab2" method="post" action="index.php">
                 <label><?php I18n::p('USERNAME'); ?></label>
-                <input name="username" type="text" class="span12">
+                <input name="username" type="text" class="span12" autocomplete="<?php echo $autocomplete;?>">
                 <label><?php I18n::p('PASSWORD'); ?></label>
-                <input name="password" type="password" class="span12" value="" autocomplete="<?php echo isset(Config::$autocomplete) && Config::$autocomplete==TRUE ?'on':'off';?>">
+                <input name="password" type="password" class="span12" value="" autocomplete="<?php echo $autocomplete;?>">
                 <?php
                 if (Config::$authentication['authentication']) {
                     ?>
                     <label><?php I18n::p('Database'); ?></label>
-                    <input name="db" type="text" class="span12" value="" autocomplete="off">
+                    <input name="db" type="text" class="span12" value="" autocomplete="<?php echo $autocomplete;?>">
                 <?php } ?>
                 <button class="btn btn-primary pull-right"><?php I18n::p('LOGIN'); ?></button>
 
