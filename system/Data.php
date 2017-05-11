da6bcb1a26a8e4c1655e6978f5f2e1cf4ec8f948
Nanhe Kumar <nanhe.kumar@gmail.com>
2017-05-11 20:05:24 +0530

    debug trace stop

--- system/Data.php
+++ system/Data.php
@@ -20,8 +20,8 @@ abstract class Data {
         if (array_key_exists($name, $this->data)) {
             return $this->data[$name];
         }
-        $trace = debug_backtrace();
-        trigger_error('Undefined property via __get(): ' . $name . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'], E_USER_NOTICE);
+        //$trace = debug_backtrace();
+        //trigger_error('Undefined property via __get(): ' . $name . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'], E_USER_NOTICE);
         return null;
     }
 
